4e647a0f47ef9ce94fee411e1da8dfc9254290c8
A <99933422+data-sandbox@users.noreply.github.com>
2023-07-26 13:30:18 -0600

    Update projects

--- readme.py
+++ readme.py
@@ -3,38 +3,34 @@ from rich.tree import Tree
 
 console = Console(record=True, width=100)
 
-tree = Tree("😀 [link=https://data-sandbox.github.io/]data-sandbox",
+tree = Tree("😀 [link=https://data-sandbox.github.io/]data-sandbox (that's me!)",
             guide_style="bold bright_black")
 
-main_tree = tree.add("📘 Main Projects", guide_style="bright_black")
-main_tree.add(
+dev_tree = tree.add("⚛️ Software Development Projects",
+                    guide_style="bright_black")
+
+dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/admin-dashboard/]dashboard[/]           - [bright_black]responsive admin dashboard (HTML, CSS)")
+dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/etch-a-sketch/]etch-a-sketch[/]       - [bright_black]drawing sketch pad (JS, HTML, CSS)")
+dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/calculator/]calculator[/]          - [bright_black]JavaScript calculator (JS, HTML, CSS)")
+dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/rock-paper-scissors/]rock-paper-scissors[/] - [bright_black]classic game of chance (JS, HTML, CSS)")
+dev_tree.add(
+    "[bold link=https://github.com/data-sandbox/data-sandbox.github.io]website[/]             - [bright_black]personal website and blog (Jekyll)")
+
+ds_tree = tree.add("📊 Data Science Projects", guide_style="bright_black")
+ds_tree.add(
     "[bold link=https://github.com/data-sandbox/nlp-brewer-finder]brewyou[/]      - [bright_black]find better breweries (NLP)")
-main_tree.add(
+ds_tree.add(
     "[bold link=https://github.com/data-sandbox/ml-bluebikes-forecasting]bluebikes[/]    - [bright_black]time series forecasting (ML)")
-main_tree.add(
-    "[bold link=https://github.com/data-sandbox/data-sandbox.github.io]website[/]      - [bright_black]personal website and blog (web)")
-
-mini_tree = tree.add("📚 Mini Projects", guide_style="bright_black")
-mini_tree.add(
+ds_tree.add(
     "[bold link=https://github.com/data-sandbox/llms/blob/main/GPT-3_chatbot.ipynb]barbot[/]       - [bright_black]custom chatbot for bar orders (LLM)")
-mini_tree.add(
+ds_tree.add(
     "[bold link=https://github.com/data-sandbox/ml-sandbox/tree/main/sentiment_amazon]sentiment[/]    - [bright_black]sentiment analysis of reviews (NLP)")
-mini_tree.add(
-    "[bold link=https://github.com/data-sandbox/data-pipelines/tree/main/bash_temp_reporting]etl[/]          - [bright_black]ETL pipeline with bash scripting")
-mini_tree.add(
-    "[bold link=https://github.com/data-sandbox/ml-sandbox/tree/main/diamond_quality]multiclass[/]   - [bright_black]multiclass classifier (ML)")
-mini_tree.add(
-    "[bold link=https://github.com/data-sandbox/restaurant-inspections]inspections[/]  - [bright_black]query NYC restaurant data (SQL)")
-mini_tree.add(
-    "[bold link=https://github.com/data-sandbox/distributed-computing]spark[/]        - [bright_black]distributing computing & clusters (Spark)")
-mini_tree.add(
+ds_tree.add(
     "[bold link=https://github.com/data-sandbox/bash/tree/main/rsync]rsync[/]        - [bright_black]backup workflow without the cloud (bash)")
-
-stats_tree = tree.add("📈 Explorations", guide_style="bright_black")
-stats_tree.add(
-    "[bold link=https://github.com/data-sandbox/stats-sandbox/blob/main/bootstrap.ipynb]bootstrap[/]    - [bright_black]powerful resampling technique (stats)")
-stats_tree.add(
-    "[bold link=https://github.com/data-sandbox/stats-sandbox/blob/main/permutation.ipynb]permutation[/]  - [bright_black]statistical significance test (stats)")
 
 console.print(tree)
 console.print("")
