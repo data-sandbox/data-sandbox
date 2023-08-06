7c0fb2f5e750de6c7b941b84fe0430cdbc846793
A <99933422+data-sandbox@users.noreply.github.com>
2023-08-06 10:33:39 -0600

    Update project list

--- readme.py
+++ readme.py
@@ -10,15 +10,15 @@ dev_tree = tree.add("⚛️ Software Development Projects",
                     guide_style="bright_black")
 
 dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/etch-a-sketch/]etch-a-sketch[/] - [bright_black]drawing sketch pad (JS, HTML, CSS)")
+dev_tree.add(
     "[bold link=https://data-sandbox.github.io/library/]library[/]       - [bright_black]store your reading list (JS, HTML, CSS)")
+dev_tree.add(
+    "[bold link=https://data-sandbox.github.io/tic-tac-toe/]tic-tac-toe[/]   - [bright_black]tic tac toe game (JS, HTML, CSS)")
 dev_tree.add(
     "[bold link=https://data-sandbox.github.io/admin-dashboard/]dashboard[/]     - [bright_black]responsive admin dashboard (HTML, CSS)")
 dev_tree.add(
-    "[bold link=https://data-sandbox.github.io/etch-a-sketch/]etch-a-sketch[/] - [bright_black]drawing sketch pad (JS, HTML, CSS)")
-dev_tree.add(
     "[bold link=https://data-sandbox.github.io/calculator/]calculator[/]    - [bright_black]JavaScript calculator (JS, HTML, CSS)")
-dev_tree.add(
-    "[bold link=https://data-sandbox.github.io/rock-paper-scissors/]r-p-s[/]         - [bright_black]rock-paper-scissors game (JS, HTML, CSS)")
 dev_tree.add(
     "[bold link=https://github.com/data-sandbox/data-sandbox.github.io]website[/]       - [bright_black]personal website and blog (Jekyll)")
 
